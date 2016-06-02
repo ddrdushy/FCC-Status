a052b00b7f87ef71f04ef03bddc551626c4ab65a
kgashok <lifebalance@gmail.com>
2016-06-02 14:36:23 +0530

    added watch back

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
     }
   };
 
-  /*grunt.loadNpmTasks('grunt-contrib-watch');*/
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.loadNpmTasks('grunt-qunit-cov');
 
