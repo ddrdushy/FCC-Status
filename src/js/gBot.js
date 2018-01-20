ad8e17ce0518c0d502fd2a7186621bf9bb2f7d04
Ajith <murukesh.jayaraj@axa.com.my>
2018-01-21 00:20:20 +0800

    Handled CORS

--- src/js/gBot.js
+++ src/js/gBot.js
@@ -1,168 +1,137 @@
-/*global $:false */
-/*jshint quotmark: false */
-/*jshint loopfunc: true */
-/*jslint latedef:false*/
-/*jslint browser:true */
-/*jshint maxparams: 5 */
 var key = "ae28f23f134c4364ad45e7b7355cfa91c92038bb";
 var arr = [];
 var points = 0;
-var html =
-    '<table class="table" id="data"><tr><th>SNO</th><th>Avatar</th><th>Name</th><th>User Name</th><th>Points</th></tr>';
+var html = '<table class="table" id="data"><thead><tr><th class="text-center">Avatar</th><th class="text-center">Name</th><th class="text-center">User Name</th><th class="text-center">Points</th></tr></thead><tbody>';
+var sum = 0;
 
-
-$(document).ready(function () {
-    "use strict";
-
-    var url = "https://api.gitter.im/v1/rooms?access_token=" + key;
+$(document).ready(function() {
+    var url = 'https://api.gitter.im/v1/rooms?access_token=' + key;
     var roomId = "";
     var noOfUsers = 0;
     $.ajax({
-        type: 'GET',
-        url: url,
+        type: 'GET', url: url,
         //data:data,
         async: false,
         dataType: 'json',
-        /*beforeSend:function(){
-            // this is where we append a loading image
-            $('#ajax-panel').html('<div class="loading"><img src="/img/loading.gif" alt="Loading..." /></div>');
-        },*/
-        success: function (data) {
+        success: function(data) {
             //Do stuff with the JSON data
-            for (var i = 0; i < data.length; i = i + 1) {
-                if (data[i].name === 'kgisl/campsite') {
-                    roomId = data[i].id;
-                    noOfUsers = data[i].userCount;
+            for (var i = 0; i < data.length; i++) { //data.length
+                if (data[i]["name"] == 'kgisl/campsite') {
+                    roomId = data[i]["id"];
+                    noOfUsers = data[i]["userCount"];
                     break;
                 }
+
             }
         },
-        error: function ( /* xhr, textStatus, errorThrown */ ) {
+        error: function(xhr, textStatus, errorThrown) {
             points = 0;
         }
     });
     var jsonData = [];
 
-
-    for (var i = 0; i < noOfUsers; i += 30) {
+    for (var i = 0; i < noOfUsers; i += 100) { //30){//noOfUsers
         $.ajax({
             type: 'GET',
-            url: 'https://api.gitter.im/v1/rooms/' + roomId +
-                '/users?access_token=' + key + '&skip=' + i,
+            url: 'https://api.gitter.im/v1/rooms/' + roomId + '/users?access_token=' + key + '&skip=' + i + '&limit=100',
             //data:data,
             async: false,
             dataType: 'json',
-            success: function (data) {
+            success: function(data) {
                 $.merge(jsonData, data);
                 //alert(jsonData);
             },
-            error: function ( /* xhr, textStatus, errorThrown */ ) {
+            error: function(xhr, textStatus, errorThrown) {
                 points = 0;
             }
         });
     }
     getData(jsonData);
+
 });
 
-
-function browniePointsFetcher(uname) {
-    "use strict";
-
-    var points = 0;
-    var url = 'https://www.freecodecamp.com/api/users/about?username=' +
-        uname.toLowerCase();
-    $.ajax({
-        type: 'GET',
-        url: url,
-        //data:data,
-        async: false,
-        dataType: 'json',
-        success: function (data) {
-            //Do stuff with the JSON data
-            points = data.about.browniePoints;
-        },
-        error: function ( /* xhr, textStatus, errorThrown */ ) {
-            points = 0;
-        }
-    });
-    return points;
-}
-
-
-function dataFormatter(image, name, uname, points) {
-    "use strict";
-
-    var tempHtml = '<td>';
-    tempHtml += '<img src=' + image +
-        ' class="img-thumbnail" width="50px" ></img></td>';
-    tempHtml += '<td>';
-    tempHtml += '<h3>' + name + '</h3></td>';
-    tempHtml += '<td>';
-    tempHtml += '<h3><a href="http://freecodecamp.com/' + uname +
-        '" target="_blank">' + uname + '</a></h3></td>';
-    tempHtml += '<td>';
-    if (points === 0) {
-        tempHtml += '<h3><span class="label label-warning">' + points +
-            '</span></h3></td>';
-    } else {
-        tempHtml += '<h3>' + points + '</h3></td>';
-    }
-
-    return tempHtml;
-}
-
+$(document).ajaxStop(function() {
+    console.log('triggered');
+    sortTable();
+});
 
 function getData(jsonData) {
-    "use strict";
-
     //alert(json["array"].length
     var len = jsonData.length;
-    var sum = 0;
-    for (var i = 0; i < len; i = i + 1) {
-        if (jsonData[i].id !== '546fc9f1db8155e6700d6e8c' &&
-            jsonData[i].id !== '5433c4b0163965c9bc209625' &&
-            jsonData[i].id !== '572db9c4c43b8c6019719c7a' &&
-            jsonData[i].id !== '570a6857187bb6f0eadec072') {
-            points = browniePointsFetcher(jsonData[i].username);
-            sum += points;
-            arr.push({
-                avatar: jsonData[i].avatarUrlSmall,
-                name: jsonData[i].displayName,
-                uname: jsonData[i].username,
-                points: points
-            });
 
-            $('.progress-bar').css({
-                width: (i / len) * 100 + '%'
-            });
+    for (var i = 0; i < len; i++) { //len
+        if (jsonData[i]["username"] !== 'QuincyLarson') {
+            browniePointsFetcher(jsonData[i]["username"]);
+
+            arr.push({avatar: jsonData[i]["avatarUrlSmall"], avatar2: jsonData[i]["avatarUrlMedium"], name: jsonData[i]["displayName"], uname: jsonData[i]["username"], points: points});
+
         }
     }
-    $('.progress-bar').prop("hidden", true);
-
-
-    arr.sort(function (a, b) {
-        return a.points - b.points;
-    });
-
-    arr.reverse();
     var j = 0;
-
-
-    html += arr.map(function (a) {
-        j = j + 1;
-        return '<tr><td>' + (j) + '</td>' + dataFormatter(a.avatar, a
-            .name, a.uname, a.points) + '</tr>';
+    html += arr.map(function(a) {
+        j++;
+        return '<tr>' + dataFormatter(a.avatar, a.name, a.uname, a.points, a.avatar2) + '</tr>';
     }).join('');
-    html += '</table>';
+    html += '</tbody></table>';
 
     $("#data").html(html);
 
-    //var a = $("#data").html();
-    $("#campers").html(
-        '<h2><span class="label label-info">Total Campers:- ' + j +
-        '</span></h2>');
-    $("#totalProblems").html(
-        '<h2><span class="label label-info">Total Problems:- ' + sum +
-        '</span></h2>');
+    var a = $("#data").html();
+    $("#campers").html('<h2><span class="label label-info">Total Campers:- ' + j + '</span></h2>');
+    $("#totalProblems").html('<h2><span class="label label-info">Total Problems:- ' + sum + '</span></h2>');
 
+}
+
+function browniePointsFetcher(uname) {
+    var points = 0;
+    var url = 'https://cors-anywhere.herokuapp.com/http://www.freecodecamp.com/api/users/about?username=' + uname.toLowerCase();
+    $.ajax({
+        type: 'GET', url: url,
+        //data:data,
+        async: true,
+        dataType: 'jsonp',
+        success: function(data) {
+            //Do stuff with the JSON data
+            points = data["about"]["browniePoints"];
+            $('#' + uname).html('<h2 class="pts">' + points + '</h2>');
+            sum += points;
+            $("#totalProblems").html('<h2><span class="label label-info">Total Problems:- ' + sum + '</span></h2>');
+
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            points = 0;
+            $('#' + uname).html('<h2 class="pts">0</h2>');
+        }
+    });
+}
+
+function dataFormatter(image, name, uname, points, urlmedium) {
+    //alert(urlmedium);
+    var temp_html = '<td>';
+    temp_html += '<img src=' + image + ' class="img-thumbnail" width="100px" ></img></td>';
+    temp_html += '<td>';
+    temp_html += '<h3>' + name + '</h3></td>';
+    temp_html += '<td>';
+    temp_html += '<h3><a href="http://freecodecamp.com/' + uname + '" target="_blank">' + uname + '</a></h3></td>';
+    temp_html += '<td id=' + uname + ' class="points"></td>';
+    return temp_html;
+}
+
+function sortTable() {
+    var rows = $('table tbody tr').get();
+
+    rows.sort(function(a, b) {
+
+        var x = parseInt($(a).children('td').eq(3).children().text());
+        var y = parseInt($(b).children('td').eq(3).children().text());
+
+        if (x < y)
+            return 1;
+        if (x > y)
+            return -1;
+        return 0;
+    });
+    $.each(rows, function(index, row) {
+        $('table').children('tbody').append(row);
+    });
 }
